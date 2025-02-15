97cce93aadbaabe77123fcc501d48e58e3960245
ElTitoJuande <juandepm00@gmail.com>
2025-02-15 21:09:09 +0100

    Footer y cards

---
+++ js/script.js
@@ -0,0 +1,68 @@
+const menuButton = document.querySelector('#mobile-menu')
+
+menuButton.addEventListener('click', e => {
+    const menu = document.querySelector('.mobile-links');
+
+    menu.classList.toggle('hidden');
+})
+
+const cursor = document.getElementById("cursor");
+let targetX = 0;
+let targetY = 0;
+
+document.addEventListener("mousemove", (e) => {
+    targetX = e.clientX;
+    targetY = e.clientY;
+});
+
+function moveCursor() {
+    const currentX = parseFloat(cursor.style.left || 0);
+    const currentY = parseFloat(cursor.style.top || 0);
+
+    const distanceX = targetX - currentX;
+    const distanceY = targetY - currentY;
+
+    cursor.style.left = `${currentX + distanceX * 0.06}px`;
+    cursor.style.top = `${currentY + distanceY * 0.06}px`;
+
+    requestAnimationFrame(moveCursor);
+}
+
+moveCursor();
+
+document.addEventListener("DOMContentLoaded", () => {
+    const observer = new IntersectionObserver((entries) => {
+        entries.forEach((entry) => {
+            if (entry.isIntersecting) {
+                entry.target.classList.add("opacity-100", "translate-y-0");
+                entry.target.classList.remove("opacity-0", "translate-y-10");
+            }
+        });
+    }, { threshold: 0.3 });
+
+    document.querySelectorAll(".fade-in").forEach((element) => {
+        observer.observe(element);
+    });
+});
+
+document.addEventListener("DOMContentLoaded", () => {
+    const carousel = document.getElementById("carousel");
+    const prevBtn = document.getElementById("prev");
+    const nextBtn = document.getElementById("next");
+  
+    if (carousel && prevBtn && nextBtn) {
+      console.log("Elementos encontrados");
+  
+      prevBtn.addEventListener("click", function () {
+        console.log("Pulsado botón prev");
+        carousel.scrollBy({ left: -300, behavior: "smooth" });
+      });
+  
+      nextBtn.addEventListener("click", function () {
+        console.log("Pulsado botón next");
+        carousel.scrollBy({ left: 300, behavior: "smooth" });
+      });
+    } else {
+      console.log("No se encontraron los elementos");
+    }
+  });
